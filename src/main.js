88c5ad914c3be10153e4087628297ca9186caa33
petraroso <proso00@fesb.hr>
2024-06-04 14:23:20 +0200

    Header and hero section

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import "../styles/modern-normalize.css"; //least important style file because it's imported first
+import "../styles/style.css";
+import "../styles/components/header.css";
+import "../styles/components/hero.css";
+import "../styles/utils.css";
